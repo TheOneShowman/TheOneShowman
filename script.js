e9f5c33fd1180e1963f783435fc28689de4a9e11
TheOneShowman <nbatson@samford.edu>
2024-10-11 12:47:41 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function downloadData() {
+  event.preventDefault();
+  var startdate = document.getElementByID("startdate").value;
+  var enddate = document.getElementByID("startdate").value;
+  alert(startdate);
+  alert(enddate);
+}
