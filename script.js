5c6c911c26a633cecf8a881ee162cac0a0dd1090
TheOneShowman <nbatson@samford.edu>
2024-10-21 12:24:22 -0500

    saftey save

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ function downloadData() {
   alert(enddate);
 fetch("https://compute.samford.edu/zohauth/clients/data");
       }
+
+
+      // // Function to fetch data and populate the table
+      async function fetchDataandPopulateTable(url) {
+        try {
+          const response = await fetch(url)
+          if (!resonpose.ok) {
+            throw new Error('Network response was not ok');
+          }
+        }
+      }
